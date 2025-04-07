ffc1e83807fc0e5962e8a05e20ecbb4647643a98
Ryan Evanyo <ryan.evanyo@gmail.com>
2025-04-07 14:03:56 -0600

    new func

--- calculate_average.py
+++ calculate_average.py
@@ -1,18 +1,34 @@
 import json
 import pandas as pd
 
-with open('average.json') as f:
-    data = json.load(f)
-    old_coins= data['coins']
-    old_average_price= data['avergae_buy_price']
+def calculate_total_average(coin):
+    with open('average.json') as f:
+        data = json.load(f)[coin]
+        old_coins= data['coins']
+        old_average_price= data['avergae_buy_price']
 
-df = pd.read_csv('bitcoin_buys.csv')
+    df = pd.read_csv(f'{coin}_buys.csv')
 
-new_coins = df['Coins'].sum()
-new_price = df['Price'].mean()
+    new_coins = df['Coins'].sum()
+    new_price = df['Price'].mean()
 
-print(new_coins)
-print(new_price)
+    weighted_avg = ((old_average_price * old_coins) + (new_price * new_coins)) / (old_coins + new_coins)
+    total_coins = old_coins+new_coins
 
-# weighted_avg = ((old_average_price * old_coins) + (new_price * new_coins)) / (old_coins + new_coins)
-# total_coins = old_coins+new_coins
+    print(f'Sum of new coins: {new_coins}')
+
+    print(f'New Average Price: {weighted_avg}')
+    print(f'Total coins owned: {total_coins}')
+
+def calculate_total_coins_owned(coin):
+    with open('average.json') as f:
+        data = json.load(f)[coin]
+        old_coins= data['coins']
+
+    df = pd.read_csv(f'{coin}_buys.csv')
+
+    new_coins = df['Coins'].sum()
+
+    total_coins = old_coins+new_coins
+
+    print(f'Total coins owned: {total_coins}')
