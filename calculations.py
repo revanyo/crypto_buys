d3e42fe0ae72116ae5da22de6054384701880757
Ryan Evanyo <ryan.evanyo@gmail.com>
2025-04-15 19:46:44 -0600

    fixed calulation funcs to work with excel

--- calculations.py
+++ calculations.py
@@ -3,49 +3,52 @@ import pandas as pd
 import requests
 
 
-# def calculate_total_average(coin):
-#     with open("data/average.json") as f:
-#         data = json.load(f)[coin]
-#         old_coins = data["coins"]
-#         old_average_price = data["avergae_buy_price"]
+def calculate_total_average(coin):
+    with open("data/average.json") as f:
+        data = json.load(f)[coin]
+        old_coins = data["coins"]
+        old_average_price = data["avergae_buy_price"]
 
-#     df = pd.read_csv(f"{coin}_buys.csv")
+    filename = f"data/{coin}_buys.xlsx"
+    df = pd.read_excel(filename)
+  
+    new_coins = df["Coins"].sum()
+    new_price = df["Price"].mean()
 
-#     new_coins = df["Coins"].sum()
-#     new_price = df["Price"].mean()
+    weighted_avg = ((old_average_price * old_coins) + (new_price * new_coins)) / (
+        old_coins + new_coins
+    )
 
-#     weighted_avg = ((old_average_price * old_coins) + (new_price * new_coins)) / (
-#         old_coins + new_coins
-#     )
+    return f"New Average Price: {weighted_avg}"
 
-#     return f"New Average Price: {weighted_avg}"
+def calculate_total_coins_owned(coin):
+    with open("data/average.json") as f:
+        data = json.load(f)[coin]
+        old_coins = data["coins"]
 
+    filename = f"data/{coin}_buys.xlsx"
+    df = pd.read_excel(filename)
 
-# def calculate_total_coins_owned(coin):
-#     with open("data/average.json") as f:
-#         data = json.load(f)[coin]
-#         old_coins = data["coins"]
+    new_coins = df["Coins"].sum()
 
-#     df = pd.read_csv(f"{coin}_buys.csv")
+    total_coins = old_coins + new_coins
 
-#     new_coins = df["Coins"].sum()
-
-#     total_coins = old_coins + new_coins
-
-#     print(f"Total coins owned: {total_coins}")
-
-
-# def calulate_new_total_coins(coin):
-#     df = pd.read_csv(f"{coin}_buys.csv")
-#     coins = df["Coins"].sum()
-#     return coins
+    print(f"Total coins owned: {total_coins}")
 
 
-# def calulate_new_average(coin):
-#     df = pd.read_csv(f"{coin}_buys.csv")
-#     new_average = df["Price"].mean()
+def calulate_new_total_coins(coin):
+    filename = f"data/{coin}_buys.xlsx"
+    df = pd.read_excel(filename)
+    coins = df["Coins"].sum()
+    return coins
 
-#     print(f"Average price for new {coin} bought: {new_average}")
+
+def calulate_new_average(coin):
+    filename = f"data/{coin}_buys.xlsx"
+    df = pd.read_excel(filename)
+    new_average = df["Price"].mean()
+
+    print(f"Average price for new {coin} bought: {new_average}")
 
 
 def get_current_price(coin):
@@ -62,18 +65,19 @@ def get_current_price(coin):
     ][0]
     return float(last_price)
 
-# def calculate_costs(coin):
-#     df = pd.read_csv(f"{coin}_buys.csv")
-#     total_cost = df["Cost"].sum()
-#     return total_cost
+def calculate_costs(coin):
+    filename = f"data/{coin}_buys.xlsx"
+    df = pd.read_excel(filename)
+    total_cost = df["Cost"].sum()
+    return total_cost
 
 
-# def calulate_profit(coin):
-#     coins = calulate_new_total_coins(coin)
-#     cost = calculate_costs(coin)
-#     current_price = get_current_price(coin)
+def calulate_profit(coin):
+    coins = calulate_new_total_coins(coin)
+    cost = calculate_costs(coin)
+    current_price = get_current_price(coin)
 
-#     current_value = coins * current_price
-#     profit = round((current_value - cost),2)
-#     percentage = round((profit/cost*100),2)
-#     return f'{profit} Dollars, {percentage}%'
+    current_value = coins * current_price
+    profit = round((current_value - cost),2)
+    percentage = round((profit/cost*100),2)
+    return f'{profit} Dollars, {percentage}%'
