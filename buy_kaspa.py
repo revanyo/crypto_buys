8b3064077c6cbdeaa7d5519b74861c3affca97f0
Ryan Evanyo <ryan.evanyo@gmail.com>
2026-01-02 12:26:29 -0700

    withdraws

--- buy_kaspa.py
+++ buy_kaspa.py
@@ -1,16 +1,24 @@
 import sys
 import datetime
 import pandas as pd
-from utils.utils import get_current_balance
+import json
+from utils.utils import get_current_balance, withdraw_asset
 
 
 def kaspa_buy():
     filename = "data/kaspa_buys.xlsx"
+    withdrawals_file = "data/kaspa_withdrawals.json"
+
     df = pd.read_excel(filename)
     coins_owned = df["Coins"].sum()
 
-    coins_bought = (float(get_current_balance("kaspa")) - 5661.903) - coins_owned
-    if(coins_bought < 10):
+    with open(withdrawals_file, "r") as f:
+        total_withdrawn = json.load(f)["total"]
+
+    coins_bought = (
+        float(get_current_balance("kaspa")) - 5661.903 + total_withdrawn
+    ) - coins_owned
+    if coins_bought < 10:
         sys.exit()
 
     last_price = 49.5 / coins_bought
@@ -23,5 +31,14 @@ def kaspa_buy():
 
     df.to_excel(filename, index=False)
 
+    row_count = len(df)
+    if row_count % 2 == 0:
+        withdraw_asset("KAS", 100)
+
+        total_withdrawn += 100
+        with open(withdrawals_file, "w") as f:
+            json.dump({"total": total_withdrawn}, f)
+
 
 kaspa_buy()
+withdraw_asset("KAS", 100)
--- utils/utils.py
+++ utils/utils.py
@@ -3,77 +3,103 @@ import time
 import requests
 from utils.kraken_auth import handle_kraken_auth
 
+
 def git_pull():
     subprocess.run(["git", "pull"], check=True)
 
+
 def get_current_balance(coin):
-    if coin.lower() == 'kaspa':
-        coin = 'KAS'
+    if coin.lower() == "kaspa":
+        coin = "KAS"
     urlpath = "/0/private/Balance"
 
-    payload = {
-        "nonce": str(int(time.time() * 1000))
-    }
+    payload = {"nonce": str(int(time.time() * 1000))}
 
     api_key, signature = handle_kraken_auth(urlpath, payload)
-    
+
     api_url = "https://api.kraken.com" + urlpath
 
     headers = {
         "API-Key": api_key,
         "API-Sign": signature,
-        "User-Agent": "kraken-api-client"
+        "User-Agent": "kraken-api-client",
     }
 
     response = requests.post(api_url, headers=headers, data=payload)
-    if(coin == "bitcoin"):
-        xbtb = (float)(response.json()['result']['XBT.B'])
-        xxbt = (float)(response.json()['result']['XXBT'])
+    if coin == "bitcoin":
+        xbtb = (float)(response.json()["result"]["XBT.B"])
+        xxbt = (float)(response.json()["result"]["XXBT"])
         return xxbt + xbtb
-    current_balance = (float)(response.json()['result'][coin])
+    current_balance = (float)(response.json()["result"][coin])
     return current_balance
 
+
 def get_allocated_balance(coin):
-    if coin == 'KAS':
+    if coin == "KAS":
         return 0
     urlpath = "/0/private/Earn/Allocations"
 
-    payload = {
-        "nonce": str(int(time.time() * 1000))
-    }
+    payload = {"nonce": str(int(time.time() * 1000))}
 
     api_key, signature = handle_kraken_auth(urlpath, payload)
-    
+
     api_url = "https://api.kraken.com" + urlpath
 
     headers = {
         "API-Key": api_key,
         "API-Sign": signature,
-        "User-Agent": "kraken-api-client"
+        "User-Agent": "kraken-api-client",
     }
 
     response = requests.post(api_url, headers=headers, data=payload).json()
-    items = response['result']['items']
-    strategies = [item for item in items if item['native_asset'] == 'BTC']
-    allocated_amount = strategies[0]['amount_allocated']['total']['native']
+    items = response["result"]["items"]
+    strategies = [item for item in items if item["native_asset"] == "BTC"]
+    allocated_amount = strategies[0]["amount_allocated"]["total"]["native"]
     return allocated_amount
+
 
 def allocate_earn_funds():
     urlpath = "/0/private/Earn/Allocate"
-    allocation_amount = get_current_balance('bitcoin') - float(get_allocated_balance('BTC'))
+    allocation_amount = get_current_balance("bitcoin") - float(
+        get_allocated_balance("BTC")
+    )
     payload = {
         "nonce": str(int(time.time() * 1000)),
         "amount": allocation_amount,
-        'strategy_id':'ESVDZB3-C3ZRV-JLKVFR'
+        "strategy_id": "ESVDZB3-C3ZRV-JLKVFR",
     }
     api_key, signature = handle_kraken_auth(urlpath, payload)
-    
+
     api_url = "https://api.kraken.com" + urlpath
 
     headers = {
         "API-Key": api_key,
         "API-Sign": signature,
-        "User-Agent": "kraken-api-client"
+        "User-Agent": "kraken-api-client",
     }
 
     requests.post(api_url, headers=headers, data=payload).json()
+
+
+def withdraw_asset(asset, amount):
+    urlpath = "/0/private/Withdraw"
+
+    payload = {
+        "nonce": str(int(time.time() * 1000)),
+        "asset": asset,
+        "key": 'kaspa_htx',
+        "amount": amount,
+    }
+
+    api_key, signature = handle_kraken_auth(urlpath, payload)
+
+    api_url = "https://api.kraken.com" + urlpath
+
+    headers = {
+        "API-Key": api_key,
+        "API-Sign": signature,
+        "User-Agent": "kraken-api-client",
+    }
+
+    response = requests.post(api_url, headers=headers, data=payload)
+    return response.json()
