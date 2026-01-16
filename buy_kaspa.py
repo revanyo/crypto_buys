9050af67d9e9a69ba02da37ff977df220c2cf962
Ryan Evanyo <ryan.evanyo@gmail.com>
2026-01-16 13:34:28 -0700

    Comment out all withdrawal logic in buy_kaspa.py

--- buy_kaspa.py
+++ buy_kaspa.py
@@ -31,14 +31,13 @@ def kaspa_buy():
 
     df.to_excel(filename, index=False)
 
-    row_count = len(df)
-    if row_count % 2 == 0:
-        withdraw_asset("KAS", 100)
+    # row_count = len(df)
+    # if row_count % 2 == 0:
+    #     withdraw_asset("KAS", 100)
 
-        total_withdrawn += 100
-        with open(withdrawals_file, "w") as f:
-            json.dump({"total": total_withdrawn}, f)
+    #     total_withdrawn += 100
+    #     with open(withdrawals_file, "w") as f:
+    #         json.dump({"total": total_withdrawn}, f)
 
 
 kaspa_buy()
-# withdraw_asset("KAS", 100)
